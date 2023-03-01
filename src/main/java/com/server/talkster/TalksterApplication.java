a9359a2042ed1c1cf56f213e39aece0da6f8bc6b
Dmytro Dzhuha <dimadjuga@gmail.com>
2023-03-01 10:27:34 +0100

    initial commit

---
+++ src/main/java/com/server/talkster/TalksterApplication.java
@@ -0,0 +1,13 @@
+package com.server.talkster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalksterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalksterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/talkster/TalksterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.talkster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalksterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
