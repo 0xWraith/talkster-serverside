6ebd29943e8e020b34a9fc10a1e8d92c8e106f1d
Dmytro Dzhuha <dimadjuga@gmail.com>
2023-03-19 01:22:20 +0100

    v0.0.4 - Chat developing

--- src/main/java/com/server/talkster/TalksterApplication.java
+++ src/main/java/com/server/talkster/TalksterApplication.java
@@ -1,12 +1,9 @@
 package com.server.talkster;
 
-import com.server.talkster.services.MailSenderService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TalksterApplication
--- src/main/java/com/server/talkster/controllers/ChatController.java
+++ src/main/java/com/server/talkster/controllers/ChatController.java
@@ -1,40 +1,32 @@
 package com.server.talkster.controllers;
 
-
-import com.server.talkster.dto.WebSocketMessage;
-import jakarta.websocket.OnClose;
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.Session;
-import jakarta.websocket.server.PathParam;
-import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
-public class ChatController {
-    private final SimpMessagingTemplate messagingTemplate;
-
+public class ChatController
+{
+    /*private final SimpMessagingTemplate simpMessagingTemplate;
 
     @Autowired
-    public ChatController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate) { this.simpMessagingTemplate = simpMessagingTemplate; }
+
+    @MessageMapping("/message") //app/message
+    @SendTo("/chatroom/public")
+    private MessageDTO receivePublicMessage(@Payload MessageDTO message)
+    {
+        System.out.println(message);
+        return message;
     }
 
-    @MessageMapping("/message")
-    public void handleMessage(@Payload WebSocketMessage message) {
-        messagingTemplate.convertAndSendToUser(message.getReceiverId(), "/queue/messages", message);
-    }
+    @MessageMapping("/private-message")
+    public MessageDTO receivePrivateMessage(@Payload MessageDTO messageDTO)
+    {
+        simpMessagingTemplate.convertAndSendToUser(messageDTO.getReceivername(), "/private", messageDTO); //user/David/private
+        return messageDTO;
+    }*/
 }
